0bfbefd6c23b117a5b6da26e0ddc965074f4daee
Mota <contato.larissamotasilva@gmail.com>
2023-09-17 09:39:10 -0300

    commit

---
+++ view/home.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VBatelie - Home</title>
+    <link rel="icon" type="image/png" href="imagens/Logo2.png">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+    <div class="container-fluid home">
+        <div class="row">
+            <div class="col-md-12 d-flex justify-content-center">
+                <div id="carouselExample" class="carousel slide m-4" data-bs-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
+                        <li data-bs-target="#carouselExample" data-bs-slide-to="1"></li>
+                        <li data-bs-target="#carouselExample" data-bs-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imagens/imagem1.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>Concertos em geral</h2>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagens/imagem2.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>Figurinos para o seu espetáculo</h2>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagens/imagem3.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>Tudo que você precisa em um só ateliê</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Próximo</span>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 text-center my-4">
+                <h2>Conheça nosso trabalho</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center text-center instagram p-2">
+            <div class="col-md-1 mx-2">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-2 mx-2 my-auto">
+                <a href="https://instagram.com/vbatelieoficial?igshid=MzNlNGNkZWQ4Mg==" target="_blank">
+                    <h2>@VBATELIE</h2>
+                    <h5>no instagram</h5>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ view/sobre.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="imagens/Logo2.png">
+    <title>VBatelie - Sobre</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid sobre">
+        <header class="row text-center py-4 mt-1 mb-4 faixa">
+            <h1>Conheça nossa história</h1>
+        </header>
+        <section class="row d-flex justify-content-center">
+            <div class="col-md-5 img1 d-flex justify-content-center">
+                <img src="imagens/sobre.jpg" alt="Imagem do Ateliê">
+            </div>
+            <div class="col-md-6">
+                <h2>Tecendo nossa história</h2>
+                <p>O ateliê "VB ateliê" surgiu quando sua criadora, Vânia Barbosa, teve a necessidade de
+                    trabalhar em casa para cuidar de seu filho e sobrinha. A princípio, ela apenas
+                    fazia consertos e trabalhava para algumas empresas. No entanto, após sofrer um
+                    grande prejuízo devido a um cliente que não a pagou corretamente, ela enxergou
+                    a possibilidade de começar a confeccionar para academias de ballet. Inicialmente,
+                    ela apenas fazia collants, mas com o tempo começou a se desafiar com figurinos de
+                    apresentações, nos quais aprendeu a confeccionar sozinha com o auxílio de alguns vídeos na internet.</p>
+                <p>Atualmente, sua maior demanda são figurinos de ballet, todavia também confecciona lindos vestidos
+                    para debutante, noivas e madrinhas, além de consertos em geral e o que mais o cliente desejar!</p>
+            </div>
+        </section>
+        <header class="row text-center my-5">
+            <h1>Como nos encontrar</h1>
+        </header>
+        <section class="row d-flex justify-content-center endereco">
+            <div class="col-md-5 d-flex justify-content-center">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.5499913070366!2d-46.40420412384351!3d-23.404491555849386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce89b34776e047%3A0x566fd6bb5d003649!2sAv.%20Bom%20Jesus%20da%20Lapa%2C%20336%20-%20Vila%20Nova%20Bonsucesso%2C%20Guarulhos%20-%20SP%2C%2007175-140!5e0!3m2!1spt-BR!2sbr!4v1689771298026!5m2!1spt-BR!2sbr"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-md-6 endereco my-auto">
+                <div class="row my-2">
+                    <div class="col-md-12">
+                        <p>Estamos localizados na Av. Bom Jesus da Lapa, 336, Bonsucesso Guarulhos-SP</p>
+                    </div>
+                </div>
+                <div class="row my-2">
+                    <div class="col-md-6 text-center text-md-start">
+                        <a href="mailto:vbatelie@gmail.com" class="me-3">vbatelie.contato@gmail.com</a>
+                    </div>
+                    <div class="col-md-6 text-center text-md-start">
+                        <a href="https://instagram.com/vbatelieoficial?igshid=MzNlNGNkZWQ4Mg==" class="me-3 d-flex justify-content-center" target="_blank">
+                            <i class="fa fa-instagram" aria-hidden="true" alt="Ícone do instagram"></i>
+                            <p class="py-1">@vbatelieoficial</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="row my-2">
+                    <p>(11) 98326-7778</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
